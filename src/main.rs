3c86bfaa58809f082e4210026519ef4e8c42e1b9
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-08-10 17:43:07 +0200

    build: extend and deny warnings, forbid unsafe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![deny(
+    warnings,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    nonstandard_style,
+    future_incompatible,
+    clippy::all
+)]
+#![forbid(unsafe_code)]
+
 fn main() {
     println!("Hello, world!");
 }
